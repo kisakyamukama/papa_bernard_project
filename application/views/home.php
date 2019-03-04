0e9eb85a18162f96eea240dcb41eabbda458cb66
Kisakyamukama <kabalimichael03@gmail.com>
2019-03-04 06:07:18 +0300

    chaninf corousel

---
+++ application/views/home.php
@@ -0,0 +1,269 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+	<head>
+		<meta charset="utf-8">
+		<title>Global Rivival league - Home</title>
+		<meta name="keywords" content="God, rivival, Jesus, Bible, saviour, Christ, heaven, help, share , love">
+		<link rel="stylesheet" href="<?php echo base_url('/assets/css/bootstrap.min.css')?>">
+		<link rel="stylesheet" href="<?php echo base_url('/assets/css/home.css')?>">
+	</head>
+	<body>
+		<!-- main -->
+					<div class="" id="main">
+						<!-- header -->
+						<header>
+											<nav class="navbar navbar-expand-lg navbar-light bg-primary text-light fixed-top">
+											  <a class="navbar-brand" href="#">Global Revival league </a>
+											  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+											    <span class="navbar-toggler-icon"></span>
+											  </button>
+												<div class="container">
+														<div class="collapse navbar-collapse" id="navbarSupportedContent">
+														<ul class="navbar-nav mr-auto">
+															<li class="nav-item active">
+																<a class="nav-link" id="home" href="#">Home <span class="sr-only">(current)</span></a>
+															</li>
+															<li class="nav-item">
+																<a class="nav-link" href="#aboutUs">About</a>
+															</li>
+															<li class="nav-item">
+																<a class="nav-link" href="#ourTeam">Our Team</a>
+															</li>
+															<li class="nav-item dropdown">
+																<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+																	projects
+																</a>
+																<div class="dropdown-menu" aria-labelledby="navbarDropdown">
+																	<a class="dropdown-item" href="#">Action</a>
+																	<a class="dropdown-item" href="#">Another action</a>
+																	<div class="dropdown-divider"></div>
+																	<a class="dropdown-item" href="#">Something else here</a>
+																</div>
+															</li>
+															<li class="nav-item">
+																<a class="nav-link" href="#" >Gallery</a>
+															</li>
+														</ul>
+														<form class="form-inline my-2 my-lg-0">
+															<input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+															<button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+														</form>
+													</div>
+												</div>
+
+
+											</nav>
+
+
+						</header>
+						<!-- header -->
+						<!-- courousel -->
+									<!-- <div class="">
+											<div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
+											  <div class="carousel-inner" style="height:320px">
+											    <div class="carousel-item active">
+											        <img src="<?php echo base_url('assets/images/54291-28950-12142015-malachi-3-1-social.800w.tn.png'); ?>" class="d-block w-100  img-fluid" alt="...">
+											    </div>
+											    <div class="carousel-item">
+											      <img src="<?php echo  base_url('assets/images/4.jpg');?>" class="d-block w-100 img-fluid" alt="...">
+											    </div>
+											    <div class="carousel-item">
+											      <img src="<?php echo  base_url('assets/images/54181-30106-cm-Restore-health-heal-wounds-social.800w.tn.png');?>" class="d-block w-100 img-fluid" alt="...">
+											    </div>
+													<div class="carousel-item">
+											      <img src="<?php echo  base_url('assets/images/praying-2179326_1920.jpg');?>" class="d-block w-100 img-fluid" alt="...">
+											    </div>
+													<div class="carousel-item">
+											      <img src="<?php echo  base_url('assets/images/sunrise-1756274_1920.jpg');?>" class="d-block w-100 img-fluid" alt="...">
+											    </div>
+													<div class="carousel-item">
+											      <img src="<?php echo  base_url('assets/images/international-2657372_1920.png');?>" class="d-block w-100 img-fluid" alt="...">
+											    </div>
+											  </div>
+											</div>
+
+						</div> -->
+						<div class="bd-example">
+						  <div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
+						    <ol class="carousel-indicators">
+						      <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
+						      <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
+						      <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
+						    </ol>
+						    <div class="carousel-inner">
+						      <div class="carousel-item active">
+						        <img src="<?php echo base_url('assets/images/54291-28950-12142015-malachi-3-1-social.800w.tn.png'); ?>" height="300px" class="d-block w-100" alt="...">
+						        <div class="carousel-caption d-none d-md-block">
+						          <h5>First slide label</h5>
+						          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+						        </div>
+						      </div>
+						      <div class="carousel-item">
+						        <img src="<?php echo  base_url('assets/images/4.jpg');?>" class="d-block w-100" alt="...">
+						        <div class="carousel-caption d-none d-md-block">
+						          <h5>Second slide label</h5>
+						          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+						        </div>
+						      </div>
+						      <div class="carousel-item">
+						        <img src="<?php echo  base_url('assets/images/54181-30106-cm-Restore-health-heal-wounds-social.800w.tn.png');?>"  class="d-block w-100" alt="...">
+						        <div class="carousel-caption d-none d-md-block">
+						          <h5>Third slide label</h5>
+						          <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+						        </div>
+						      </div>
+						    </div>
+						    <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
+						      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+						      <span class="sr-only">Previous</span>
+						    </a>
+						    <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
+						      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+						      <span class="sr-only">Next</span>
+						    </a>
+						  </div>
+						</div>
+						<!-- courousel -->
+
+						<!-- main section -->
+							<section class="container main-view">
+
+								<!-- cards -->
+
+								<div class="card-columns mt-4">
+									<div class="card">
+									 <div class="card-body">
+										 	<h5 class="card-title">Mission</h5>
+										 	<p class="card-text">This is another card with title and supporting text below. This card has some additional content to make it slightly taller overall.</p>
+											<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+											More about our mission
+											</button>
+										 </div>
+								 </div>
+
+									 <div class="card">
+										<div class="card-body">
+											<h5 class="card-title">Vision</h5>
+											<p class="card-text">This is another card with title and supporting text below. This card has some additional content to make it slightly taller overall.</p>
+											<a href="#" class="btn btn-primary">More</a>
+										</div>
+									</div>
+
+										<div class="card">
+										 <div class="card-body">
+											 <h5 class="card-title">Currently</h5>
+											 <p class="card-text">This is another card with title and supporting text below. This card has some additional content to make it slightly taller overall.</p>
+											<a href="#" class="btn btn-primary">Details</a>
+
+										 </div>
+								     </div>
+								    </div>
+
+
+								<!-- cards -->
+								<!-- implementation of card button -->
+								<!-- Button trigger modal -->
+
+
+														<!-- Modal  for the mission button-->
+														<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+														<div class="modal-dialog" role="document">
+														<div class="modal-content">
+															<div class="modal-header">
+																<h5 class="modal-title" id="exampleModalLabel">Our mission details</h5>
+																<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+																	<span aria-hidden="true">&times;</span>
+																</button>
+															</div>
+															<div class="modal-body">
+																...
+															</div>
+															<div class="modal-footer">
+																<button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+																<!-- <button type="button" class="btn btn-primary">Save changes</button> -->
+															</div>
+														</div>
+														</div>
+														</div>
+								<!-- implementation of card button -->
+								<hr>
+								<h4 id="ourTeam" class="text-center">Team</h4>
+								<div class="row card-columns mt-4	">
+									<div class="card" style="width: 16rem;">
+										  <img src="<?php echo  base_url('assets/images/2.jpg');?>" class="card-img-top" alt="...">
+										  <div class="card-body">
+										    <p class="card-text">
+										    	<h3>Manager</h3>
+													<p>Team leader and director</p>
+													<p>Contact: <b>kabalimichael032gmail.com</b> </p>
+													<p> <b>+256797268242</b> </p>
+										    </p>
+										  </div>
+									</div>
+									<div class="card" style="width: 16rem;">
+										  <img src="<?php echo base_url('assets/images/24991190_529370957441754_3241362637225147642_n.jpg')?>" class="card-img-top" alt="...">
+										  <div class="card-body">
+										    <p class="card-text">
+													<h3>Secretary</h3>
+													<p>Content management</p>
+													<p>Contact: <b>michael032gmail.com</b> </p>
+													<p> <b>+256753307626</b> </p>
+												</p>
+												</p>
+										  </div>
+									</div>
+									<div class="card" style="width: 16rem;">
+										<img src="<?php echo base_url('assets/images/44559870_179440356270568_391925979192229888_n.jpg')?>" class="card-img-top" alt="...">
+										 <div class="card-body">
+												<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+											</div>
+									</div>
+									<div class="card" style="width: 16rem;">
+										<img src="<?php echo base_url('assets/images/44559870_179440356270568_391925979192229888_n.jpg')?>" class="card-img-top" alt="...">
+										 <div class="card-body">
+												<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+											</div>
+									</div>
+								</div>
+
+
+
+								<hr>
+								<h4 id="aboutUs">About us</h4>
+
+
+
+
+
+
+
+
+							</section>
+						<!-- main  section-->
+
+						<!-- footer -->
+						<footer>
+							<div class="mt-5 mb-0" id="footer">
+								<div class="jumbotron">
+									<div class="text-center">
+										&copy; 2019
+
+									</div>
+
+								</div>
+							</div>
+
+
+
+					</div>
+		<!-- main -->
+		</footer>
+		<!-- footer -->
+
+
+	  <!-- javascript -->
+		<script src="<?php echo base_url('assets/jquery/jquery.min.js');?>"></script>
+		<script src="<?php echo base_url('assets/js/bootstrap.min.js');?>"></script>
+
+	</body>
+</html>
